dbe766e39ff26e6e60eb0a12edc776f9a01b3abe
Abdulloh072008 <abdullohbilol54@gmail.com>
2026-07-13 16:21:12 +0500

    changed

--- lib/config.ts
+++ lib/config.ts
@@ -1,8 +1,7 @@
 // Central config for the Instagram clone frontend.
 // Backend: https://instagram-api.softclub.tj/swagger/index.html
 
-export const API_BASE =
-  process.env.NEXT_PUBLIC_API_URL ?? "https://instagram-api.softclub.tj";
+export const API_BASE = process.env.NEXT_PUBLIC_API_URL;
 export const IMAGE_BASE = `${API_BASE}/images`;
 
 /** Build a full URL for an image filename returned by the API. */
